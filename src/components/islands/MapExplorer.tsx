449ec87be320a777342057fb2abe5e857bb75f32
crllect <crllect@proton.me>
2026-08-08 12:15:03 -0400

    map

--- src/components/islands/MapExplorer.tsx
+++ src/components/islands/MapExplorer.tsx
@@ -1,5 +1,23 @@
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+const US_STATES_TOPOJSON = "/us-states-10m.json";
+
 const MapExplorer = () => (
-	<div class="mapExplorer" role="img" aria-label="Map placeholder" />
+	<div class="mapExplorer">
+		<ComposableMap projection="geoAlbersUsa" width={975} height={610}>
+			<Geographies geography={US_STATES_TOPOJSON}>
+				{({ geographies }) =>
+					geographies.map(geography => (
+						<Geography
+							key={geography.rsmKey}
+							geography={geography}
+							className="mapState"
+						/>
+					))
+				}
+			</Geographies>
+		</ComposableMap>
+	</div>
 );
 
 export default MapExplorer;
